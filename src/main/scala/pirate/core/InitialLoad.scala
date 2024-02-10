6d38663dccea14802c319f4e551925a098750210
Dave Smith <davesmith.00000@gmail.com>
2024-02-10 21:30:09 +0000

    All background anims are clips

--- src/main/scala/pirate/core/InitialLoad.scala
+++ src/main/scala/pirate/core/InitialLoad.scala
@@ -6,25 +6,25 @@ import indigo.shared.formats.TiledGridMap
 import pirate.generated.Assets.*
 import pirate.generated.CaptainAnim
 
-/*
-In a nutshell, the setup function here takes the boot data (screen dimensions),
-the asset collection, and a dice object, and produces "start up data", which is
-totally user defined and you can do that however you like, you just need to return
-a success or failure object.
-
-What's really important to understand is that this function is run _more than once!_
-
-The first time it runs, we only have available the assets we told indigo we needed
-for the loading screen. We find this out by simply checking which assets are available
-at the moment.
-
-The second run is triggered by the completion of a dynamic asset load - you see the
-progress of which on the loading screen. This can theoretically happen as many times
-as you decide to load assets. So it's only on the second run that we do all the work
-in `makeAdditionalAssets`.
- */
 object InitialLoad:
 
+  /*
+  In a nutshell, the setup function here takes the boot data (screen dimensions),
+  the asset collection, and a dice object, and produces "start up data", which is
+  totally user defined and you can do that however you like, you just need to return
+  a success or failure object.
+
+  What's really important to understand is that this function is run _more than once!_
+
+  The first time it runs, we only have available the assets we told indigo we needed
+  for the loading screen. We find this out by simply checking which assets are available
+  at the moment.
+
+  The second run is triggered by the completion of a dynamic asset load - you see the
+  progress of which on the loading screen. This can theoretically happen as many times
+  as you decide to load assets. So it's only on the second run that we do all the work
+  in `makeAdditionalAssets`.
+   */
   def setup(
       screenDimensions: Rectangle,
       assetCollection: AssetCollection,
@@ -52,24 +52,20 @@ object InitialLoad:
       screenDimensions: Rectangle,
       assetCollection: AssetCollection,
       dice: Dice
-  ): Option[(LevelDataStore, List[Animation])] = {
-
-    val loader: (AssetName, AssetName, Depth) => Either[String, SpriteAndAnimations] =
-      loadAnimation(assetCollection, dice)
+  ): Option[LevelDataStore] =
+    val singleClipLoader: (AssetName, AssetName, String) => Either[String, Clip[Material.Bitmap]] =
+      loadSingleClip(assetCollection, dice)
 
     // If these assets haven't been loaded yet, we're not going to try and process anything.
-    if (
-      assetCollection.findTextDataByName(assets.helm.ShipHelmData).isDefined &&
+    if assetCollection.findTextDataByName(assets.helm.ShipHelmData).isDefined &&
       assetCollection.findTextDataByName(assets.trees.PalmTreeData).isDefined &&
       assetCollection.findTextDataByName(assets.water.WaterReflectData).isDefined &&
       assetCollection.findTextDataByName(assets.flag.FlagData).isDefined &&
       assetCollection.findTextDataByName(assets.static.terrainData).isDefined
-    ) {
-
-      val tileMapper: Int => TileType = {
+    then
+      val tileMapper: Int => TileType =
         case 0 => TileType.Empty
         case _ => TileType.Solid
-      }
 
       // Here we read the Tiled level description and manufacture a triple of:
       // (the tile size, a `TiledGridMap` of data, and a renderable verison of the map)
@@ -82,75 +78,123 @@ object InitialLoad:
         } yield (Point(tileMap.tilewidth, tileMap.tileheight), grid, terrainGroup.withDepth(Depth(4)))
 
       for {
-        helm <- loader(
+        helm <- singleClipLoader(
           assets.helm.ShipHelmData,
           assets.helm.ShipHelm,
-          Depth(9)
+          "Idle"
         ).toOption
-        palm <- loader(
+        palm <- singleClipLoader(
           assets.trees.PalmTreeData,
           assets.trees.PalmTree,
-          Depth(1)
+          "P Front"
         ).toOption
-        reflections <- loader(
+        backPalm <- singleClipLoader(
+          assets.trees.PalmTreeData,
+          assets.trees.PalmTree,
+          "P Back"
+        ).toOption
+        reflections <- singleClipLoader(
           assets.water.WaterReflectData,
           assets.water.WaterReflect,
-          Depth(20)
+          "Big"
         ).toOption
-        flag    <- loader(assets.flag.FlagData, assets.flag.Flag, Depth(10)).toOption
+        flag <- singleClipLoader(
+          assets.flag.FlagData,
+          assets.flag.Flag,
+          "Flapping"
+        ).toOption
         terrain <- terrainData
-      } yield makeAdditionalAssets(screenDimensions, helm, palm, reflections, flag, terrain._1, terrain._2, terrain._3)
-    } else None
-  }
+      } yield makeAdditionalAssets(
+        screenDimensions,
+        helm,
+        palm,
+        backPalm,
+        reflections,
+        flag,
+        terrain._1,
+        terrain._2,
+        terrain._3
+      )
+    else None
 
   // Helper function that loads Aseprite animations.
   def loadAnimation(
       assetCollection: AssetCollection,
       dice: Dice
-  )(jsonRef: AssetName, name: AssetName, depth: Depth): Either[String, SpriteAndAnimations] = {
+  )(jsonRef: AssetName, name: AssetName, depth: Depth): Either[String, SpriteAndAnimations] =
     val res = for {
       json                <- assetCollection.findTextDataByName(jsonRef)
       aseprite            <- Json.asepriteFromJson(json)
       spriteAndAnimations <- aseprite.toSpriteAndAnimations(dice, name)
     } yield spriteAndAnimations.copy(sprite = spriteAndAnimations.sprite.withDepth(depth))
 
-    res match {
+    res match
       case Some(spriteAndAnimations) =>
         Right(spriteAndAnimations)
 
       case None =>
         Left("Failed to load " + name)
+
+  // Helper function that loads Aseprite clips.
+  def loadClip(
+      assetCollection: AssetCollection,
+      dice: Dice
+  )(jsonRef: AssetName, name: AssetName): Either[String, Map[CycleLabel, Clip[Material.Bitmap]]] =
+    val res = for {
+      json     <- assetCollection.findTextDataByName(jsonRef)
+      aseprite <- Json.asepriteFromJson(json)
+      clips    <- aseprite.toClips(name)
+    } yield clips
+
+    res match
+      case Some(clips) =>
+        Right(clips)
+
+      case None =>
+        Left(s"Failed to load $name")
+
+  // Helper function that loads a single Aseprite clip.
+  def loadSingleClip(
+      assetCollection: AssetCollection,
+      dice: Dice
+  )(jsonRef: AssetName, name: AssetName, cycleName: String): Either[String, Clip[Material.Bitmap]] =
+    loadClip(assetCollection, dice)(jsonRef, name).flatMap {
+      _.get(CycleLabel(cycleName)) match
+        case Some(clip) =>
+          Right(clip)
+
+        case None =>
+          Left(s"No cycle named: $cycleName for asset: $name")
     }
-  }
 
   def makeAdditionalAssets(
       screenDimensions: Rectangle,
-      helm: SpriteAndAnimations,
-      palm: SpriteAndAnimations,
-      waterReflections: SpriteAndAnimations,
-      flag: SpriteAndAnimations,
+      helm: Clip[Material.Bitmap],
+      palm: Clip[Material.Bitmap],
+      backPalm: Clip[Material.Bitmap],
+      waterReflections: Clip[Material.Bitmap],
+      flag: Clip[Material.Bitmap],
       tileSize: Point,
       terrainMap: TiledGridMap[TileType],
       terrain: Group
-  ): (LevelDataStore, List[Animation]) =
-    (
-      LevelDataStore(
-        waterReflections.sprite
-          .withRef(85, 0)
-          .moveTo(screenDimensions.horizontalCenter, screenDimensions.verticalCenter + 5),
-        flag.sprite.withRef(22, 105).moveTo(200, 288),
-        helm.sprite.withRef(31, 49).moveTo(605, 160),
-        palm.sprite,
-        tileSize,
-        terrainMap,
-        terrain
-      ),
-      List(waterReflections.animations, flag.animations, helm.animations, palm.animations)
+  ): LevelDataStore =
+    LevelDataStore(
+      waterReflections
+        .withDepth(Depth(20))
+        .withRef(85, 0)
+        .moveTo(screenDimensions.horizontalCenter, screenDimensions.verticalCenter + 5),
+      flag.withDepth(Depth(9)).withRef(22, 105).moveTo(200, 288),
+      helm.withRef(31, 49).moveTo(605, 160),
+      palm.withDepth(Depth(1)),
+      backPalm.withDepth(Depth(10)),
+      tileSize,
+      terrainMap,
+      terrain
     )
 
   def makeStartupData(
       captain: SpriteAndAnimations,
-      levelDataStore: Option[(LevelDataStore, List[Animation])]
+      levelDataStore: Option[LevelDataStore]
   ): Startup.Success[StartupData] =
     Startup
       .Success(
@@ -159,29 +203,25 @@ object InitialLoad:
             .modifyMaterial(m => Material.ImageEffects(m.diffuse))
             .withRef(37, 64)
             .moveTo(300, 271),
-          levelDataStore.map(_._1)
+          levelDataStore
         )
       )
       .addAnimations(captain.animations)
-      .addAnimations(levelDataStore.map(_._2).getOrElse(Nil))
 
 final case class StartupData(
     captain: Sprite[Material.ImageEffects],
     levelDataStore: Option[LevelDataStore]
 )
 final case class LevelDataStore(
-    waterReflections: Sprite[Material.Bitmap],
-    flag: Sprite[Material.Bitmap],
-    helm: Sprite[Material.Bitmap],
-    palm: Sprite[Material.Bitmap],
+    waterReflections: Clip[Material.Bitmap],
+    flag: Clip[Material.Bitmap],
+    helm: Clip[Material.Bitmap],
+    palm: Clip[Material.Bitmap],
+    backTallPalm: Clip[Material.Bitmap],
     tileSize: Point,
     terrainMap: TiledGridMap[TileType],
     terrain: Group
-):
-  val backTallPalm: Sprite[Material.Bitmap] =
-    palm
-      .withBindingKey(BindingKey("Back Tall Palm"))
-      .withDepth(Depth(10))
+)
 
 enum TileType:
   case Empty, Solid
