04d6956049f9a256c01afba7939a6b712e7e0c23
Dave Smith <davesmith.00000@gmail.com>
2024-02-12 23:35:16 +0000

    OOPS: Fixed pirate restitution

--- src/main/scala/pirate/scenes/level/model/LevelModel.scala
+++ src/main/scala/pirate/scenes/level/model/LevelModel.scala
@@ -102,6 +102,7 @@ object LevelModel:
         .addColliders(
           Collider
             .Box("pirate", Pirate.initialBounds(spaceConvertors))
+            .withRestitution(Restitution(0))
         )
         .addColliders(
           Collider
